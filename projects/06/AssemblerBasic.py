598a031c3d9e1e9f3d3c586968dd7aeb82c2b856
Solomon Bothwell <ssbothwell@gmail.com>
2017-01-15 19:23:17 -0800

    identifed A vs C instructions and converts A instructions to binary

--- projects/06/AssemblerBasic.py
+++ projects/06/AssemblerBasic.py
@@ -3,8 +3,25 @@ import os, sys
 
 file = open(sys.argv[1], 'r')
 asmArray = []
+
+# Load assembly file and iterate line by line
+# removing all white space, comments, and linebreaks
+# loading actual code into asmArray list.
 for line in file:
     if not line.startswith('//') and line != '\n':
         asmArray.append(line.rstrip())
 
+# A-instructions begin with `@` character
+# C-instructions begin with an opcode, destcode
+# jump code, or a semicolon
+
+
+for index, instruction in enumerate(asmArray):
+    # if it is an A-instruction then convert to a 16bit number
+    if instruction.startswith('@'):
+        asmArray[index] = ('{0:016b}'.format(int(instruction[1:])), 'A')
+    else:
+        asmArray[index] = (instruction, 'C')
+
 print(asmArray)
+
